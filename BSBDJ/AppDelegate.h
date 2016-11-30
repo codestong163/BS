201c196d822ccd7894ac92aa96675db31a325d2d
Lanhongjian <137142763@qq.com>
2016-11-30 13:45:41 +0800

    Initial Commit

---
+++ BSBDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ
+//
+//  Created by Hongjian.Lan on 2016/11/30.
+//  Copyright © 2016年 Hongjian.Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSBDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSBDJ
+//
+//  Created by Hongjian.Lan on 2016/11/30.
+//  Copyright © 2016年 Hongjian.Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
